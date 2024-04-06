31b6395ed672a31f577be4039dfcfa874c4a5ee3
anlbe <anl.berky123@gmail.com>
2024-04-06 16:27:38 +0300

    Update meeting info and enable audit properties
    The meeting information display has been updated to show all meetings instead of one in the Home views. New meeting data has been added to the Repository model.

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -2,23 +2,74 @@ namespace WebApplication1.Models;
 
 public static class Repository
 {
+    private static List<MeetingInfo> _meeting = new();
+    
     private static List<UserInfo> _users = new();
 
     static Repository()
     {
-        _users.Add(new UserInfo()
+        _meeting.Add((new MeetingInfo()
         {
-            Id = 1,Name = "Ahmet" , Email = "abcf@gmail.com", Phone = "22222",WillAttend = true
+            MeetingId = 1,
+            MeetingLocation = "Meeting Room 1",
+            MeetingDate = DateTime.Now.AddHours(1),
+            MeetingNumberOfPeople = 10
+        }));
+        _meeting.Add((new MeetingInfo()
+        {
+            MeetingId = 2,
+            MeetingLocation = "Meeting Room 2",
+            MeetingDate = DateTime.Now.AddHours(2),
+            MeetingNumberOfPeople = 10
+        }));
+        _meeting.Add((new MeetingInfo()
+        {
+            MeetingId = 3,
+            MeetingLocation = "Meeting Room 3",
+            MeetingDate = DateTime.Now.AddHours(3),
+            MeetingNumberOfPeople = 10
+        }));
+        _meeting.Add(new MeetingInfo()
+        {
+            MeetingId = 4,
+            MeetingLocation = "Meeting Room 4",
+            MeetingDate = DateTime.Now.AddHours(4),
+            MeetingNumberOfPeople = 10
         });
         _users.Add(new UserInfo()
         {
-            Id = 2, Name = "Mehmet", Email = "abce@gmail.com", Phone = "444444", WillAttend = false
+            Id = 1,
+            Name = "Ahmet",
+            Email = "abcf@gmail.com",
+            Phone = "22222",
+            WillAttend = true
         });
         _users.Add(new UserInfo()
         {
-            Id = 3, Name = "Ceylan", Email = "abcd@gmail.com", Phone = "333333", WillAttend = true
+            Id = 2,
+            Name = "Mehmet",
+            Email = "abce@gmail.com",
+            Phone = "444444",
+            WillAttend = false
+        });
+        _users.Add(new UserInfo()
+        {
+            Id = 3,
+            Name = "Ceylan",
+            Email = "abcd@gmail.com",
+            Phone = "333333",
+            WillAttend = true
         });
     }
+
+    public static List<MeetingInfo> Meetings
+    {
+        get
+        {
+           return _meeting;
+        }
+    }
+    
     public static List<UserInfo> Users
     {
         get
